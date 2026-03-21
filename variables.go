042902a5f4a80e90ed0a43f72eae89430dd8fb57
tejutejas05 <tejasmr05@gmail.com>
2026-03-21 22:52:48 +0530

    zero-value variable

--- variables.go
+++ variables.go
@@ -2,15 +2,16 @@ package main
 
 import ("fmt")
 
+
 func main() {
 
-	var i int = 25
-	fmt.Println(i)
+	//var i int = 25
+	//fmt.Println(i)
 
 	j:=10
 	fmt.Println(j)
 
-	fmt.Printf(" %v , %T \n", i ,i) // where %v ---> return the values of the variables
+	//fmt.Printf(" %v , %T \n", i ,i) // where %v ---> return the values of the variables
 	fmt.Printf(" %v , %T \n", j ,j) // where %T ---> returns the datatype of the variables
 
 	// we can assign many variables at a time 
@@ -20,6 +21,9 @@ func main() {
 	//	prashi := 69 ----> We can't use the := method while declaring the many variables in the var( )
 		prashi int = 69 // Now it will run because it is in the correct format ( varkeyword Variable_name Datatype)
 	)
+
+	var i int 
+	println(i) // it will print the default value of the int which is 0
 
 	fmt.Println(tejas , jevita , prashi)
 }
