0ac06188689a4c7c31962f9d61e3ba6201e6bf9e
tejutejas05 <tejasmr05@gmail.com>
2026-03-21 23:18:01 +0530

    Constants in GO

---
+++ constants.go
@@ -0,0 +1,15 @@
+package main
+
+import ( "fmt" )
+
+const PI = 3.14
+
+func main() {
+
+	const tej = "Hello From Tej!!"
+	fmt.Println("Hiii?" , tej)
+
+	fmt.Println("what is the constant value of the PI" , PI)
+
+	
+}
