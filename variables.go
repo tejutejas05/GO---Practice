ce1e90aeb2b2dbed2c20e352644b2d3044ec7f74
tejutejas05 <tejasmr05@gmail.com>
2026-03-21 23:00:07 +0530

    Multiple-variables in Same and Different Type

--- variables.go
+++ variables.go
@@ -8,22 +8,36 @@ func main() {
 	//var i int = 25
 	//fmt.Println(i)
 
-	j:=10
-	fmt.Println(j)
+	//j:=10
+	//fmt.Println(j)
 
 	//fmt.Printf(" %v , %T \n", i ,i) // where %v ---> return the values of the variables
-	fmt.Printf(" %v , %T \n", j ,j) // where %T ---> returns the datatype of the variables
+	//fmt.Printf(" %v , %T \n", j ,j) // where %T ---> returns the datatype of the variables
 
 	// we can assign many variables at a time 
-	var (
-		tejas string = "hello tejas"
-		jevita string = "hello jev"
+	//var (
+	//	tejas string = "hello tejas"
+	//	jevita string = "hello jev"
 	//	prashi := 69 ----> We can't use the := method while declaring the many variables in the var( )
-		prashi int = 69 // Now it will run because it is in the correct format ( varkeyword Variable_name Datatype)
-	)
+	//	prashi int = 69 // Now it will run because it is in the correct format ( varkeyword Variable_name Datatype)
+	
 
-	var i int 
-	println(i) // it will print the default value of the int which is 0
+	//var i int 
+	//println(i) // it will print the default value of the int which is 0
 
-	fmt.Println(tejas , jevita , prashi)
+	//fmt.Println(tejas , jevita , prashi)
+
+	//Declaring the Multiple variables in a Same Type
+	var a,b,c int = 1,2,3
+	fmt.Printf(" %v , %T \n", a ,a)
+	fmt.Printf(" %v , %T \n", b ,b)
+	fmt.Printf(" %v , %T \n", c ,c)
+
+	//Declaring the Multiple variables in a Diferent Type
+	var d,e,f = 1,"tejas",3.14
+	fmt.Printf(" %v , %T \n", d ,d)
+	fmt.Printf(" %v , %T \n", e ,e)
+	fmt.Printf(" %v , %T \n", f ,f)
+
+
 }
