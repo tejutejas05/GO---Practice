cab0fee0e382a9b423dfaa5e93683c59d7d1d9a0
tejutejas05 <tejasmr05@gmail.com>
2026-03-22 16:25:02 +0530

    Decalring the new functions and calling it in the main function

---
+++ functions.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	tejas()
+}
+
+//Decalring the new functions and calling it in the main function 
+func tejas(){
+	fmt.Println("hello from the tejas")
+}
