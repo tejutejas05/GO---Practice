fb9c10ad3e69b222f658ac985c87de25723cc223
tejutejas05 <tejasmr05@gmail.com>
2026-03-23 19:45:04 +0530

    conditional statements'

--- Basic_Projects/quiz_game.go
+++ Basic_Projects/quiz_game.go
@@ -17,7 +17,25 @@ func main() {
 	fmt.Scan(&age)
 	if age < 18 {
 		fmt.Println("You are not eligible to play our game")
+		return // using the return it will end the fuction (if user comes to else condition then program will stop)
 	} else {
 		fmt.Println("You can play the game")
 	}
+
+	fmt.Printf("Which is the better programming language: GO or Python --> ")
+
+	var answer string
+	fmt.Scan(&answer)
+
+	fmt.Println(answer)
+
+	 
+	// Conditions to check the if answer is correct or not
+	if answer == "GO"{
+		fmt.Println("Correct Answer!!")
+	} else{
+		fmt.Println("Wrong answer")
+	}
+
 }
+
